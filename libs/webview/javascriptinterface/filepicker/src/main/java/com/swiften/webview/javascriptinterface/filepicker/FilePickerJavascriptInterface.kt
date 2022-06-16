ef64e80d189483df3e3a909f649bf8bdfcfd1e46
Hai Pham <swiften.svc@gmail.com>
2022-06-17 03:14:56 +0800

    feat(webview): read more file data on file pick

--- libs/webview/javascriptinterface/filepicker/src/main/java/com/swiften/webview/javascriptinterface/filepicker/FilePickerJavascriptInterface.kt
+++ libs/webview/javascriptinterface/filepicker/src/main/java/com/swiften/webview/javascriptinterface/filepicker/FilePickerJavascriptInterface.kt
@@ -3,8 +3,10 @@ package com.swiften.webview.javascriptinterface.filepicker
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.provider.OpenableColumns
 import android.webkit.JavascriptInterface
-import androidx.fragment.app.Fragment
+import android.webkit.MimeTypeMap
 import com.swiften.commonview.genericlifecycle.IGenericLifecycleOwner
 import com.swiften.commonview.genericlifecycle.NoopGenericLifecycleOwner
 import com.swiften.commonview.activityresult.IActivityResultEventHook
@@ -16,8 +18,6 @@ import com.swiften.webview.IJavascriptInterface
 import com.swiften.webview.parseArguments
 import com.swiften.webview.processStream
 import io.reactivex.Completable
-import io.reactivex.Single
-import io.reactivex.processors.BehaviorProcessor
 import io.reactivex.processors.PublishProcessor
 import java.util.concurrent.atomic.AtomicReference
 
@@ -25,6 +25,8 @@ class FilePickerJavascriptInterface(
   override val name: String,
   private val activityResultLauncher: Lazy<IActivityResultLauncher<PickFileInput, PickFileOutput>>,
   private val argsParser: Lazy<BridgeMethodArgumentsParser>,
+  private val context: Lazy<Context>,
+  private val mimeTypeMap: Lazy<MimeTypeMap> = LazyProperty(initialValue = MimeTypeMap.getSingleton()),
   private val requestProcessor: Lazy<BridgeRequestProcessor>,
 ) : IJavascriptInterface,
   IGenericLifecycleOwner by NoopGenericLifecycleOwner
@@ -36,9 +38,23 @@ class FilePickerJavascriptInterface(
   }
 
   sealed class MethodResult {
-    data class PickFile(val requestID: String, val uri: String?) : MethodResult() {
+    data class PickFile(
+      val requestID: String,
+      val extension: String?,
+      val mimeType: String?,
+      val name: String?,
+      val size: Long?,
+      val uri: String?,
+    ) : MethodResult() {
       companion object {
-        internal val NOOP = PickFile(requestID = REQUEST_ID_NOOP, uri = null)
+        internal val NOOP = PickFile(
+          requestID = REQUEST_ID_NOOP,
+          extension = null,
+          mimeType = null,
+          name = null,
+          size = null,
+          uri = null,
+        )
       }
     }
   }
@@ -84,12 +100,45 @@ class FilePickerJavascriptInterface(
             }
 
             override fun parseResult(resultCode: Int, intent: Intent?): PickFileOutput {
-              val uri = intent?.dataString
+              val rawUri = intent?.dataString
 
-              if (resultCode == Activity.RESULT_OK && uri != null) {
+              if (resultCode == Activity.RESULT_OK && rawUri != null) {
+                val contentResolver = this@FilePickerJavascriptInterface.context.value.contentResolver
+                val uri = Uri.parse(rawUri)
+                val mimeType = contentResolver.getType(uri)
+
+                val extension = this@FilePickerJavascriptInterface.mimeTypeMap.value
+                  .getExtensionFromMimeType(mimeType)
+
+                var fileName: String? = null
+                var fileSize: Long? = null
+
+                /**
+                 * We are only given permission to read the file metadata right here, so we must
+                 * acquire as much information as we can.
+                 */
+                contentResolver.query(
+                  uri,
+                  null,
+                  null,
+                  null,
+                  null,
+                )?.let { cursor ->
+                  val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                  val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                  cursor.moveToFirst()
+                  fileName = cursor.getString(nameIndex)
+                  fileSize = cursor.getLong(sizeIndex)
+                  cursor.close()
+                }
+
                 return PickFileOutput(
                   requestID = this@FilePickerJavascriptInterface.activeRequestID.get(),
-                  uri = uri,
+                  extension = extension,
+                  mimeType = mimeType,
+                  name = fileName,
+                  size = fileSize,
+                  uri = rawUri,
                 )
               }
 
