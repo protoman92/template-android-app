db0771ec00ab71861a3d9f0dcf1d5996593ce78f
Hai Pham <swiften.svc@gmail.com>
2022-07-03 22:56:47 +0800

    feat(webview): add javascript interface method to read a file's contents

--- libs/webview/javascriptinterface/fileopener/src/main/java/com/swiften/webview/javascriptinterface/fileopener/FileOpenerJavascriptInterface.kt
+++ libs/webview/javascriptinterface/fileopener/src/main/java/com/swiften/webview/javascriptinterface/fileopener/FileOpenerJavascriptInterface.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Base64
 import android.webkit.JavascriptInterface
 import com.swiften.commonview.activity.IActivityStarter
 import com.swiften.commonview.lifecycle.IGenericLifecycleOwner
@@ -14,7 +15,10 @@ import com.swiften.webview.IJavascriptInterface
 import com.swiften.webview.parseArguments
 import com.swiften.webview.processStream
 import io.reactivex.Completable
+import io.reactivex.Single
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class FileOpenerJavascriptInterface(
   override val name: String,
@@ -30,8 +34,17 @@ class FileOpenerJavascriptInterface(
 ) : IJavascriptInterface,
   IGenericLifecycleOwner by NoopGenericLifecycleOwner
 {
-  sealed class MethodArguments {
-    data class OpenFile(val mimeType: String, val uri: String) : MethodArguments()
+  sealed interface MethodArguments {
+    data class OpenFile(val mimeType: String, val uri: String) : MethodArguments
+
+    data class ReadFile(val encoding: Encoding, val uri: String) : MethodArguments {
+      companion object {
+        @Suppress("EnumEntryName")
+        enum class Encoding {
+          base64
+        }
+      }
+    }
   }
 
   @SuppressLint("SetWorldReadable")
@@ -71,6 +84,51 @@ class FileOpenerJavascriptInterface(
           Completable.complete()
         } catch (error: Exception) {
           Completable.error(error)
+        }
+      },
+      bridgeArguments = request,
+    )
+  }
+
+  @JavascriptInterface
+  fun readFile(rawRequest: String) {
+    val request = this.argsParser.value.parseArguments<MethodArguments.ReadFile>(rawRequest)
+
+    this.requestProcessor.value.processStream(
+      stream = Single.defer {
+        val internalStorageDir = this@FileOpenerJavascriptInterface.context.value.filesDir
+        val internalStorageRawUri = Uri.fromFile(internalStorageDir).toString()
+        val rawUri = request.parameters.uri
+        var inputStream: InputStream? = null
+
+        try {
+          val uri = Uri.parse(rawUri)
+
+          inputStream = if (request.parameters.uri.startsWith(internalStorageRawUri)) {
+            val filePath = request.parameters.uri
+              .substring(internalStorageRawUri.length)
+              .removePrefix("/")
+
+            this@FileOpenerJavascriptInterface.context.value.openFileInput(filePath)
+          } else {
+            this@FileOpenerJavascriptInterface.context.value.contentResolver.openInputStream(uri)
+          }
+
+          if (inputStream == null) {
+            throw FileNotFoundException("$rawUri does not point to a valid file")
+          }
+
+          val contents = when (request.parameters.encoding) {
+            MethodArguments.ReadFile.Companion.Encoding.base64 -> {
+              Base64.encodeToString(inputStream.readBytes(), Base64.NO_WRAP)
+            }
+          }
+
+          Single.just(contents)
+        } catch (error: Exception) {
+          Single.error(error)
+        } finally {
+          inputStream?.close()
         }
       },
       bridgeArguments = request,
