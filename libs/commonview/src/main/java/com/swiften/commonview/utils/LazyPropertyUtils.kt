5e7700730ec0427a5a4c2d9fce6e5e8b429d26ab
Hai Pham <swiften.svc@gmail.com>
2022-07-10 13:05:55 +0800

    feat(commonview): allow map extension in Lazy instead of ReadonlyLazyProperty

--- libs/commonview/src/main/java/com/swiften/commonview/utils/LazyPropertyUtils.kt
+++ libs/commonview/src/main/java/com/swiften/commonview/utils/LazyPropertyUtils.kt
@@ -1,7 +1,7 @@
 package com.swiften.commonview.utils
 
-fun <T1: Any, T2 : Any> IReadonlyLazyProperty<T1>.map(mapper: (T1) -> T2): IReadonlyLazyProperty<T2> {
-  return object : IReadonlyLazyProperty<T2> {
+fun <T1: Any, T2 : Any> Lazy<T1>.map(mapper: (T1) -> T2): Lazy<T2> {
+  return object : Lazy<T2> {
     override val value: T2 get() {
       return mapper(this@map.value)
     }
